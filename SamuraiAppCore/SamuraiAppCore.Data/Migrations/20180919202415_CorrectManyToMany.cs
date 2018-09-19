26911a365e893498ad7a07a807cf1facb9009dfa
aferrercrafter <aferrercrafter@gmail.com>
2018-09-19 17:27:42 -0300

    Corrected Many To Many Relationship....maybe

---
+++ SamuraiAppCore/SamuraiAppCore.Data/Migrations/20180919202415_CorrectManyToMany.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SamuraiAppCore.Data.Migrations
+{
+    public partial class CorrectManyToMany : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Samurais_Battles_BattleId",
+                table: "Samurais");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Samurais_BattleId",
+                table: "Samurais");
+
+            migrationBuilder.DropColumn(
+                name: "BattleId",
+                table: "Samurais");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "BattleId",
+                table: "Samurais",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Samurais_BattleId",
+                table: "Samurais",
+                column: "BattleId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Samurais_Battles_BattleId",
+                table: "Samurais",
+                column: "BattleId",
+                principalTable: "Battles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
