29d5afa702a973c5eb43860d359bbb606763dc3e
aferrercrafter <aferrercrafter@gmail.com>
2018-09-19 17:18:16 -0300

    Added NetFramework App for course purposes

---
+++ SamuraiApp/SamuraiApp.Data/SamuraiContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SamuraiApp.Domain;
+
+namespace SamuraiApp.Data
+{
+    public class SamuraiContext : DbContext
+    {
+        public DbSet<Samurai> Samurais { get; set; }
+        public DbSet<Quote> Quotes { get; set; }
+        public DbSet<SecretIdentity> SecretIdentities { get; set; }
+        public DbSet<Battle> Battles { get; set; }
+        public DbSet<SamuraiBattle> SamuraiBattles { get; set; }
+
+        protected override void OnModelCreating (ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<SamuraiBattle>().HasKey(x => new { x.SamuraiId, x.BattleId });
+        }
+
+        protected override void OnConfiguring (DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "Data Source = (localdb)\\MSSQLLocalDB; Database = SamuraiDataCore; Integrated Security = True; Connect Timeout = 30; Encrypt = False; TrustServerCertificate = True; ApplicationIntent = ReadWrite; MultiSubnetFailover = False"
+                );
+        }
+
+    }
+}
---
+++ SamuraiApp/SamuraiApp.Domain/Samurai.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace SamuraiApp.Domain
+{
+    public class Samurai
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }        
+        public SecretIdentity SecretIdentity { get; set; }
+        public List<Quote> Quotes { get; set; }
+        public List<SamuraiBattle> SamuraiBattles {get;set;}
+
+    }
+}
