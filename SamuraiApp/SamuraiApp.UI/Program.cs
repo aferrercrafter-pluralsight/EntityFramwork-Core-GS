17972f9e0a35bf6eb1a753ffafd72ebc5bf5ce96
aferrercrafter <aferrercrafter@gmail.com>
2018-09-20 13:06:31 -0300

    Adding UI Project for experiments purposes.

---
+++ SamuraiApp/SamuraiApp.UI/Program.cs
@@ -0,0 +1,24 @@
+﻿using SamuraiApp.Domain;
+using SamuraiApp.Data;
+
+namespace SamuraiApp.UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InsertSamurai();
+        }
+
+        public static void InsertSamurai()
+        {
+            var samurai = new Samurai { Name = "Julie" };
+            using (var context = new SamuraiContext())
+            {
+                context.Samurais.Add(samurai);
+                //contexxt.Add(samurai);
+                context.SaveChanges();
+            }
+        }
+    }
+}
