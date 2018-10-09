a8e841a909e381007bada5d333abc48001d12cce
LAPTOP-JH6UP2RU\Arnaldo Corzo <aferrercrafter@gmail.com>
2018-10-08 22:40:16 -0300

    Using EF Core with WPF

---
+++ SamuraiApp/SamuraiApp.Data/ConnectedData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SamuraiApp.Data
+{
+    public class ConnectedData
+    {
+        private SamuraiContext _context;
+
+        public ConnectedData()
+        {
+            _context = new SamuraiContext();
+        }
+        
+    }
+}
