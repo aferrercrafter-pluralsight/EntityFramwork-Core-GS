20c96d9c83ba38a21a7ee488581bf7f00ca97ee0
aferrercrafter <aferrercrafter@gmail.com>
2018-10-10 18:09:33 -0300

    EF Core With WPF Fixing xaml

--- SamuraiApp/SamuraiApp.Data/ConnectedData.cs
+++ SamuraiApp/SamuraiApp.Data/ConnectedData.cs
@@ -1,8 +1,7 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using SamuraiApp.Domain;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SamuraiApp.Data
 {
@@ -14,6 +13,36 @@ namespace SamuraiApp.Data
         {
             _context = new SamuraiContext();
         }
-        
+
+        public IEnumerable<Samurai> SamuraisListInMemory()
+        {
+            if (_context.Samurais.Local.Count == 0)
+            {
+                _context.Samurais.ToList();
+            }
+            return _context.Samurais.Local.ToBindingList();
+        }
+
+        public Samurai LoadSamuraiGraph(int samuraiId)
+        {
+            var samurai = _context.Samurais.Find(samuraiId);
+            _context.Entry(samurai).Reference(s => s.SecretIdentity).Load();
+            _context.Entry(samurai).Collection(s => s.Quotes).Load();
+
+            return samurai;
+        }
+
+        public Samurai NewSamurai()
+        {
+            var samurai = new Samurai() { Name = "New Samurai" };
+            _context.Samurais.Add(samurai);
+            return samurai;
+        }
+
+        public void SaveChanges()
+        {
+            _context.SaveChanges();
+        }
+
     }
 }
--- SamuraiApp/SamuraiApp.WpfUi/MainWindow.xaml.cs
+++ SamuraiApp/SamuraiApp.WpfUi/MainWindow.xaml.cs
@@ -1,17 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using SamuraiApp.Data;
+using SamuraiApp.Domain;
 
 namespace SamuraiApp.WpfUi
 {
@@ -20,6 +11,11 @@ namespace SamuraiApp.WpfUi
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly ConnectedData _repo = new ConnectedData();
+        private Samurai _currentSamurai;
+        private bool _isListChanging;
+        private bool _isLoading;
+        private ObjectDataProvider _samuraiViewSource;
         public MainWindow()
         {
             InitializeComponent();
@@ -27,10 +23,72 @@ namespace SamuraiApp.WpfUi
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
+            _isLoading = true;            
+            samuraiListBox.ItemsSource = _repo.SamuraisListInMemory();
+            _samuraiViewSource = (ObjectDataProvider)FindResource("SamuraiViewSource");
+            _isLoading = false;
+            samuraiListBox.SelectedIndex = 0;            
+        }
 
-            System.Windows.Data.CollectionViewSource samuraiViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("samuraiViewSource")));
-            // Load data by setting the CollectionViewSource.Source property:
-            // samuraiViewSource.Source = [generic data source]
+        private void SamuraiDataGrid_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            if(!_isLoading)
+            {
+                _isListChanging = true;
+                _currentSamurai = _repo.LoadSamuraiGraph((int)samuraiListBox.SelectedValue);
+                _samuraiViewSource.ObjectInstance = _currentSamurai;
+                _isListChanging = false;
+            }
+        }
+
+        public void SaveChanges()
+        {
+            _repo.SaveChanges();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            _currentSamurai = _repo.NewSamurai();
+            _samuraiViewSource.ObjectInstance = _currentSamurai;
+            samuraiListBox.SelectedItem = _currentSamurai;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            SaveChanges();
+        }
+
+        private void RealName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if(!_isLoading && !_isListChanging)
+            {
+                if(_currentSamurai.SecretIdentity is null)
+                {
+                    _currentSamurai.SecretIdentity = new SecretIdentity();
+                }
+                _currentSamurai.SecretIdentity.RealName = ((TextBox)sender).Text;
+                _currentSamurai.IsDirty = true;
+            }
+        }
+        private void Name_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!_isLoading && !_isListChanging)
+            {
+                _currentSamurai.IsDirty = true;
+            }
+        }
+
+        private void Name_LostFocus(object sender, RoutedEventArgs e)
+        {
+            samuraiListBox.Items.Refresh();
+        }
+
+        private void Quotes_CellEditing(object sender, DataGridCellEditEndingEventArgs e)
+        {
+            if(!_isLoading && !_isListChanging)
+            {
+                _currentSamurai.IsDirty = true;
+            }
         }
     }
 }
